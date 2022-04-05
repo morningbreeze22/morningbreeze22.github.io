df023874baabe5ee8a737d179390d2ba160d72af
JIAWEI SONG <jiaweis@usc.edu>
2022-04-04 19:19:18 -0700

    hw3

--- asdsadassadasdsjw3/hw3-react/src/App.js
+++ asdsadassadasdsjw3/hw3-react/src/App.js
@@ -6,8 +6,6 @@ import Scenemap from './Scenemap';
 import Webmap from './Webmap';
 import MapController from "./MapController"
 
-let ismap = 0;
-
 function App() {
   ReactDOM.render(
     <MapController /> ,
@@ -18,24 +16,3 @@ function App() {
 }
 
 export default App;
-
-/*function App(){
-  ReactDOM.render(
-    <div style={{ width: '100vw', height: '100vh' }}  
-        viewProperties={{
-          center: [-118.28538,34.0220],
-          zoom: 15
-    }}>
-  
-    <WebMap id='d8160e88e9604a14a2624f37f4f33d30'>
-    <Campus/>
-    </WebMap>
-  
-    </div>,
-    document.getElementById('container')
-  );  
-}
-
-export default App;*/
-  
-  
